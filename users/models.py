970dd4a18352944896c0dc10e4bc7f05239262f1
hanwool77 <wjsgksdnf11@naver.com>
2020-09-11 18:36:35 +0900

    #16.4

--- users/models.py
+++ users/models.py
@@ -6,36 +6,24 @@ from django.core.mail import send_mail
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
 
-# Create your models here.
+
 class User(AbstractUser):
     """ Custom User Model """
 
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
-
     GENDER_CHOICES = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
-
     LANGUAGE_ENGLISH = "en"
     LANGUAGE_KOREAN = "kr"
-
-    LANGUAGE_CHOICES = (
-        (LANGUAGE_ENGLISH, "English"),
-        (LANGUAGE_KOREAN, "Korean"),
-    )
-
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
-
-    CURRENCY_CHOICES = (
-        (CURRENCY_USD, "USD"),
-        (CURRENCY_KRW, "KRW"),
-    )
-
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
@@ -48,7 +36,7 @@ class User(AbstractUser):
     )
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
-    email_secret = models.CharField(max_length=20, default=False, blank=True)
+    email_secret = models.CharField(max_length=20, default="", blank=True)
 
     def verify_email(self):
         if self.email_verified is False:
@@ -65,4 +53,5 @@ class User(AbstractUser):
                 fail_silently=False,
                 html_message=html_message,
             )
+            self.save()
         return
